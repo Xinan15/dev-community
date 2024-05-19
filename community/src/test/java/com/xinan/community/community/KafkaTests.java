2329c1c59e9dc7f4a2ba90d1831faf25aa08018b
Xinan <youkingan11@gmail.com>
2024-05-19 18:01:07 +0100

    ***Imp*** integrating Spring with Kafka - 1

---
+++ community/src/test/java/com/xinan/community/community/KafkaTests.java
@@ -0,0 +1,17 @@
+package com.xinan.community.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+    @Test
+    public void testKafka() {
+
+    }
+}
